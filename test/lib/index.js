cce7443f871f6f5c2844328dd697c731a156df66
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2016-07-21 14:57:50 +0700

    we can pass array of tasks, then they can run in parallel

--- test/lib/index.js
+++ test/lib/index.js
@@ -178,6 +178,142 @@ test('sequence of tasks + hard error', t => {
     });
 });
 
+test('parallel of tasks + resolve', t => {
+    const testSpy1 = spy();
+    const testSpy2 = spy();
+
+    function task1() {
+        return function testTask1() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    testSpy1();
+                    resolve();
+                }, 100);
+            });
+        };
+    }
+    function task2() {
+        return function testTask2() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    testSpy1();
+                    resolve();
+                }, 90);
+            });
+        };
+    }
+    function task3() {
+        return function testTask3() {
+            return new Promise((resolve, reject) => {
+                testSpy2();
+                resolve();
+            });
+        };
+    }
+
+    start(noopReporter)([ task1, task2 ], task3)
+    .then(() => {
+        t.ok(
+            testSpy1.calledTwice,
+            'task 1 and task 2 must been called once for each'
+        );
+        t.ok(
+            testSpy2.calledOnce,
+            'task 3 must been called once'
+        );
+        t.ok(
+            testSpy2.calledAfter(testSpy1),
+            'task 3 must been called after task 1 and 2'
+        );
+
+        t.end();
+    });
+});
+
+test('parallel of tasks + resolve with value', t => {
+    const testSpy1 = spy();
+    const testSpy2 = spy();
+
+    const value1 = 1;
+    const value2 = 2;
+
+    function task1() {
+        return function testTask1() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    testSpy1();
+                    resolve(value1);
+                }, 100);
+            });
+        };
+    }
+    function task2() {
+        return function testTask2() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    testSpy1();
+                    resolve(value2);
+                }, 90);
+            });
+        };
+    }
+    function task3(input) {
+        return function testTask3() {
+            return new Promise((resolve, reject) => {
+                testSpy2(...input);
+                resolve();
+            });
+        };
+    }
+
+    start(noopReporter)([ task1, task2 ], task3)
+    .then(() => {
+        t.ok(testSpy1.calledTwice, 'task 1 and task 2 must been called once for each');
+        t.ok(testSpy2.calledOnce, 'task 3 must been called once');
+        t.ok(testSpy2.calledAfter(testSpy1), 'task 3 must been called after task 1 and 2');
+        t.ok(testSpy2.calledWith(value1, value2), 'task3 must been called with proper values');
+        t.end();
+    });
+});
+
+test('parallel of tasks + reject', t => {
+    const testSpy1 = spy();
+    const testSpy2 = spy();
+
+    function task1() {
+        return function testTask1() {
+            return new Promise((resolve, reject) => {
+                testSpy1();
+                reject();
+            });
+        };
+    }
+    function task2() {
+        return function testTask2() {
+            return new Promise((resolve, reject) => {
+                testSpy1();
+                reject();
+            });
+        };
+    }
+    function task3() {
+        return function testTask3() {
+            return new Promise((resolve, reject) => {
+                testSpy2();
+                resolve();
+            });
+        };
+    }
+
+    start(noopReporter)([ task1, task2 ], task3)
+    .catch(() => {
+        t.ok(testSpy1.calledTwice, 'task 1 and task 2 must been called once for each');
+        t.ok(!testSpy2.calledOnce, 'task 3 must not been called');
+        t.end();
+    });
+
+});
+
 test('nested', t => {
     const testSpy1 = spy();
     const testSpy2 = spy();
