e25b68396866b019f9be13fe79635b1e3ab2269c
Kir Belevich <kir@belevi.ch>
2018-03-18 16:42:17 +0100

    :boom: :wrench: @start/cli: expect options to be passed to `start` field in `package.json`

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,51 +1,34 @@
 #!/usr/bin/env node
+
 // @flow
 /* eslint-disable no-process-exit */
-import commander from 'commander'
-import resolveCwd from 'resolve-cwd'
+import importCwd from 'import-cwd'
 
-const collect = (value, result) => result.concat(value)
+const rootPackage = importCwd('./package.json')
 
-commander
-  .usage('[options] <task runner> [arguments]')
-  .option('-f, --file <file>', 'tasks file path', 'start.js')
-  .option('-p, --preset <preset>', 'tasks preset')
-  .option('-r, --require [module]', 'module to require (repeatable)', collect, [])
-  .parse(process.argv)
-
-let modulePath = null
-
-if (commander.preset) {
-  modulePath = resolveCwd(commander.preset)
-
-  if (!modulePath) {
-    console.error(`Unable to find "${commander.preset}" preset`)
-    process.exit(1)
-  }
-} else {
-  modulePath = resolveCwd(`./${commander.file}`)
-
-  if (!modulePath) {
-    console.error(`Unable to find "${commander.file}" file`)
-    process.exit(1)
-  }
+const options = {
+  file: 'tasks',
+  ...rootPackage.start,
 }
 
-// commander.require.forEach(require)
+if (Array.isArray(options.require)) {
+  options.require.forEach(require)
+}
 
-const tasks = require(modulePath)
+let tasks = options.preset ? importCwd(options.preset) : importCwd(`./${options.file}`)
 
 const getAvailableTasksRunnersMessage = () => {
   return `Available task runners: "${Object.keys(tasks).join('", "')}"`
 }
 
-if (commander.args.length === 0) {
-  console.log('Task is required')
+const taskName = process.argv[2]
+
+if (typeof taskName === 'undefined') {
+  console.log('Task runner name is required')
   console.log(getAvailableTasksRunnersMessage())
   process.exit(0)
 }
 
-const taskName = commander.args[0]
 const taskRunner = tasks[taskName]
 
 if (typeof taskRunner === 'undefined') {
@@ -54,7 +37,7 @@ if (typeof taskRunner === 'undefined') {
   process.exit(1)
 }
 
-const taskArgs = commander.args.slice(1)
+const taskArgs = process.argv.slice(3)
 
 taskRunner(...taskArgs)({ taskName }).catch(() => {
   process.exit(1)
