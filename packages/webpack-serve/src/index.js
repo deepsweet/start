1e6b9217170a09305476fcb78601682873e4e52c
Kir Belevich <kir@belevi.ch>
2018-03-18 16:36:56 +0100

    :wrench: @start/webpack-serve: remove useless log

--- packages/webpack-serve/src/index.js
+++ packages/webpack-serve/src/index.js
@@ -5,13 +5,7 @@ export default (options?: {}) => {
   const webpackServe: StartPlugin = ({ input, logMessage }) => {
     const serve = require('webpack-serve')
 
-    return serve(options).then((server) => {
-      server.on('listening', () => {
-        logMessage('up and running')
-      })
-
-      return input
-    })
+    return serve(options).then(() => input)
   }
 
   return webpackServe
