06f6d0b4bce7cd9d3a0131cf641f6a6a9d630aa1
Kir Belevich <kir@belevi.ch>
2018-03-18 21:57:40 +0100

    :heavy_plus_sign: add `overwrite` plugin

---
+++ packages/overwite/src/index.js
@@ -0,0 +1,59 @@
+// @flow
+import type { StartPlugin } from '@start/task/src/'
+
+const overwrite: StartPlugin = ({ input, logPath }) => {
+  const path = require('path')
+  const makethen = require('makethen')
+  const gracefulFs = require('graceful-fs')
+  const writeFile = makethen(gracefulFs.writeFile)
+
+  return Promise.all(
+    input.map((file) => {
+      // file.path = /Users/foo/test/packages/beep/src/boop/index.js
+      const writeFiles = []
+      let fileData = file.data || ''
+
+      // sourcemap
+      if (file.map != null) {
+        // /Users/foo/test/packages/beep/src/boop/index.js -> /Users/foo/test/packages/beep/src/boop/
+        const inDir = path.dirname(file.path)
+        // /Users/foo/test/packages/beep/src/boop/index.js -> index.js
+        const inFile = path.basename(file.path)
+        // /beep/boop/src/beep/index.js -> .js
+        const inExtname = path.extname(file.path)
+        // index.js -> index.js.map
+        const sourcemapFile = inFile + '.map'
+        // /beep/boop/build/beep/index.js -> /beep/boop/build/beep/index.js.map
+        const sourcemapPath = path.join(inDir, sourcemapFile)
+        const sourcemapData = JSON.stringify(file.map)
+
+        // /*# sourceMappingURL=index.css.map */
+        if (inExtname === '.css') {
+          fileData += '\n/*# sourceMappingURL='
+          fileData += sourcemapFile
+          fileData += ' */'
+          // //# sourceMappingURL=index.js.map
+        } else {
+          fileData += '\n//# sourceMappingURL='
+          fileData += sourcemapFile
+        }
+
+        writeFiles.push(
+          writeFile(sourcemapPath, sourcemapData, 'utf8').then(() => {
+            logPath(sourcemapPath)
+          })
+        )
+      }
+
+      writeFiles.push(
+        writeFile(file.path, fileData, 'utf8').then(() => {
+          logPath(file.path)
+        })
+      )
+
+      return Promise.all(writeFiles)
+    })
+  ).then(() => input)
+}
+
+export default overwrite
