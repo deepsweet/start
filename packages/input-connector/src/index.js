56104625c926590950d47a650e85e7856e4a4bdd
Kir Belevich <kir@belevi.ch>
2018-03-18 21:53:37 +0100

    :boom: :wrench: @start/input-connector: always expect array of files

--- packages/input-connector/src/index.js
+++ packages/input-connector/src/index.js
@@ -1,19 +1,19 @@
 // @flow
-import type { StartPlugin, StartInput } from '@start/task/src/'
+import type { StartPlugin } from '@start/task/src/'
 
-export default (arg: string | StartInput) => {
-  const inputConnector: StartPlugin = () => {
-    if (typeof arg === 'string') {
-      return [
-        {
-          path: arg,
-          data: null,
-          map: null,
-        },
-      ]
-    }
+export default (files: string[]) => {
+  const inputConnector: StartPlugin = ({ logPath }) => {
+    const path = require('path')
 
-    return arg
+    return files.map((file) => {
+      logPath(file)
+
+      return {
+        path: path.resolve(file),
+        data: null,
+        map: null,
+      }
+    })
   }
 
   return inputConnector
