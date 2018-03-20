5c84a28f50f6e2fbf64eeef9eb4726ca74e84613
Kir Belevich <kir@belevi.ch>
2018-03-20 19:55:04 +0100

    :heavy_plus_sign: add `@start/plugin-flow-check`

---
+++ packages/plugin-flow-check/src/index.js
@@ -0,0 +1,22 @@
+// @flow
+import type { StartPlugin } from '@start/task/src/'
+
+export default (...flowArgs: string[]) => {
+  const flowCheck: StartPlugin = () => {
+    const path = require('path')
+    const execa = require('execa')
+
+    const flowBinPath = path.resolve('node_modules/.bin/flow')
+
+    return execa('node', [flowBinPath, 'check', ...flowArgs], {
+      stdout: process.stdout,
+      stderr: process.stderr,
+      stripEof: false,
+      env: {
+        FORCE_COLOR: true,
+      },
+    }).catch(() => Promise.reject())
+  }
+
+  return flowCheck
+}
