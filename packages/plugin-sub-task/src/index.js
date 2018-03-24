3980173f2b8d4c97d9a47c997731e75478047de8
Kir Belevich <kir@belevi.ch>
2018-03-24 16:08:52 +0100

    :construction_worker: :boom: delete `plugin-sub-task`

--- packages/plugin-sub-task/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import type { StartTaskRunner, StartPlugin } from '@start/task/src/'
-
-export default (taskRunner: StartTaskRunner) => (...args: *[]) => {
-  const subTask: StartPlugin = ({ input, taskName }) =>
-    taskRunner(...args)({ input, taskName: `${taskName}.${taskRunner.name}` })
-
-  return subTask
-}
