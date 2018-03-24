a91eb5756c0595f76984ec2682cc88f58be38a90
Kir Belevich <kir@belevi.ch>
2018-03-24 16:08:23 +0100

    :construction_worker: :boom: rename `plugin-parallel` to `parallel`

---
+++ packages/parallel/src/index.js
@@ -0,0 +1,26 @@
+// @flow
+import type { StartPlugin } from '@start/sequence/src/'
+
+type StartTask = (...args: any[]) => StartPlugin
+
+export default (options?: {}) => (...tasks: StartTask[]) => (...args: string[]) => () => {
+  const execa = require('execa')
+
+  const spawnOptions = {
+    stdout: process.stdout,
+    stderr: process.stderr,
+    stripEof: false,
+    env: {
+      FORCE_COLOR: true,
+    },
+  }
+
+  return Promise.all(
+    tasks.map((task) => {
+      const spawnCommand = process.argv[0]
+      const spawnArgs = [process.argv[1], task.name, ...process.argv.slice(3)]
+
+      return execa(spawnCommand, spawnArgs, spawnOptions).catch(() => Promise.reject())
+    })
+  )
+}
