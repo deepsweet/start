41bc8e876db2351196476a01751bcae14dd60585
Kir Belevich <kir@belevi.ch>
2018-03-25 05:57:11 +0200

    :heavy_plus_sign: add `xargs`

---
+++ packages/xargs/src/index.js
@@ -0,0 +1,26 @@
+// @flow
+import type { StartPlugin } from '@start/sequence/src/'
+
+type StartTask = (...args: string[]) => StartPlugin
+
+export default (task: StartTask) => (...input) => () => {
+  const execa = require('execa')
+
+  const spawnOptions = {
+    stdout: process.stdout,
+    stderr: process.stderr,
+    stripEof: false,
+    env: {
+      FORCE_COLOR: true,
+    },
+  }
+
+  return Promise.all(
+    input.map((arg) => {
+      const spawnCommand = process.argv[0]
+      const spawnArgs = [process.argv[1], task.name, arg]
+
+      return execa(spawnCommand, spawnArgs, spawnOptions).catch(() => Promise.reject())
+    })
+  )
+}
