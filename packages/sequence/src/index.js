ad4a197dd231005bdfdcfb8d6122924aa372d123
Kir Belevich <kir@belevi.ch>
2018-03-25 18:29:10 +0200

    :construction_worker: sequence: pipe middlewares from outside if needed

--- packages/sequence/src/index.js
+++ packages/sequence/src/index.js
@@ -16,17 +16,17 @@ export type StartPlugin = (StartPluginArg) => Promise<StartInput> | StartInput
 
 export type StartMiddleware = (StartPlugin) => (StartPluginArg) => Promise<StartInput> | StartInput
 
-const sequence = (...middlewares: StartMiddleware[]) => (...plugins: StartPlugin[]) => ({
+const sequence = (middleware: StartMiddleware) => (...plugins: StartPlugin[]) => ({
   input = [],
   taskName,
 }: StartPluginArg) =>
-  plugins.reduce(async (result, plugin) => {
-    const enhancedPlugin = middlewares.reduce((a, b) => b(a), plugin)
-
-    return enhancedPlugin({
-      input: await result,
-      taskName,
-    })
-  }, input)
+  plugins.map(middleware).reduce(
+    async (result, plugin) =>
+      plugin({
+        input: await result,
+        taskName,
+      }),
+    input
+  )
 
 export default sequence
