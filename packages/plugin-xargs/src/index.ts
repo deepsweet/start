3ab6a199bf996d9d23969f7e1d3078c11b71923e
Kir Belevich <kir@belevi.ch>
2018-04-01 19:39:33 +0200

    🛠 xargs: add `concurrency` option

--- packages/plugin-xargs/src/index.ts
+++ packages/plugin-xargs/src/index.ts
@@ -2,8 +2,15 @@ import { StartPlugin } from '@start/plugin-sequence'
 
 type StartTask = (...args: string[]) => StartPlugin
 
-export default (task: StartTask) => (...args: string[]): StartPlugin => async ({ input }) => {
+type Options = {
+  concurrency?: number
+}
+
+export default (task: StartTask, options: Options = {}) => (
+  ...args: string[]
+): StartPlugin => async ({ input }) => {
   const { default: execa } = await import('execa')
+  const { default: pAll } = await import('p-all')
 
   const spawnOptions = {
     stdout: process.stdout,
@@ -13,13 +20,18 @@ export default (task: StartTask) => (...args: string[]): StartPlugin => async ({
       FORCE_COLOR: '1',
     },
   }
+  const pAllOptions = {
+    concurrency: Infinity,
+    ...options,
+  }
 
-  return Promise.all(
+  return pAll(
     args.map((arg) => {
       const spawnCommand = process.argv[0]
       const spawnArgs = [process.argv[1], task.name, arg]
 
-      return execa(spawnCommand, spawnArgs, spawnOptions).catch(() => Promise.reject(null))
-    })
+      return () => execa(spawnCommand, spawnArgs, spawnOptions).catch(() => Promise.reject(null))
+    }),
+    pAllOptions
   ).then(() => input)
 }
