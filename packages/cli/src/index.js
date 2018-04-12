2c654e4fb291e90064279f710761c126cf8642a4
Kir Belevich <kir@belevi.ch>
2018-04-12 19:10:46 +0200

    ✔️ cli: fix requires and imports

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -2,33 +2,30 @@
 /* eslint-disable import/unambiguous */
 /* eslint-disable no-process-exit */
 /* eslint-disable no-global-assign */
-const importCwd = require('import-cwd')
-const rootPackage = importCwd('./package.json')
+const { resolve } = require('path')
+const { start: options } = require(resolve('./package.json'))
 
-const options = {
-  file: 'tasks',
-  require: [],
-  ...rootPackage.start
-}
 const ESM_SYMBOL = Symbol.for('esm\u200D:package')
 
-options.require.forEach((pkg) => {
-  if (typeof pkg === 'string') {
-    const required = importCwd(pkg)
+if (Array.isArray(options.require)) {
+  options.require.forEach((pkg) => {
+    if (typeof pkg === 'string') {
+      const required = require(pkg)
 
-    if (required[ESM_SYMBOL]) {
-      require = required(module)
-    }
-  } else if (Array.isArray(pkg)) {
-    const required = importCwd(pkg[0])
+      if (required[ESM_SYMBOL]) {
+        require = required(module)
+      }
+    } else if (Array.isArray(pkg)) {
+      const required = require(pkg[0])
 
-    if (required[ESM_SYMBOL]) {
-      require = required(module, pkg[1])
-    } else {
-      required(pkg[1])
+      if (required[ESM_SYMBOL]) {
+        require = required(module, pkg[1])
+      } else {
+        required(pkg[1])
+      }
     }
-  }
-})
+  })
+}
 
 const { default: cliLib } = require('./lib')
 
--- packages/cli/src/lib.ts
+++ packages/cli/src/lib.ts
@@ -1,14 +1,21 @@
-import importCwd from 'import-cwd'
+import { resolve } from 'path'
 
 type Options = {
   file?: string,
   preset?: string,
   reporter?: string,
+  // TODO: harden me
   require?: Array<string | any[]>
 }
 
 export default async (argv: string[], options: Options) => {
-  let tasks = importCwd(options.preset || `./${options.file}`)
+  if (!options.reporter) {
+    throw '`reporter` option is missing in your `package.json` → `start`'
+  }
+
+  const tasksFile = options.file || 'tasks'
+  const tasksToRequire = options.preset || resolve(`./${tasksFile}`)
+  const tasks = require(tasksToRequire)
   const taskName = argv[2]
   const task = tasks[taskName]
 
@@ -17,11 +24,6 @@ export default async (argv: string[], options: Options) => {
   }
 
   const taskArgs = argv.slice(3)
-
-  if (!options.reporter) {
-    throw '`reporter` option is missing in your `package.json` → `start`'
-  }
-
   const { default: reporter } = await import(options.reporter)
 
   return task(...taskArgs)({ reporter: reporter(taskName) })
