257856059a86f3dd11593e432f4ac0448ae3239d
Kir Belevich <kir@belevi.ch>
2018-04-14 17:41:54 +0200

    🛠️ cli: print list of tasks as list

--- packages/cli/src/lib.ts
+++ packages/cli/src/lib.ts
@@ -20,7 +20,7 @@ export default async (argv: string[], options: Options) => {
   const task = tasks[taskName]
 
   if (typeof taskName === 'undefined' || typeof task === 'undefined') {
-    throw `One of the following task names is required: "${Object.keys(tasks).join('", "')}"`
+    throw `One of the following task names is required:\n* ${Object.keys(tasks).join('\n* ')}`
   }
 
   const taskArgs = argv.slice(3)
