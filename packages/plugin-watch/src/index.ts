68be9c7ca00cb351a177a755cdf3f20bb5288a83
Kir Belevich <kir@belevi.ch>
2018-04-25 18:30:27 +0200

    💥🛠️ plugin-watch: handle only `add` and `change` events

--- packages/plugin-watch/src/index.ts
+++ packages/plugin-watch/src/index.ts
@@ -2,65 +2,65 @@
 /* eslint-disable promise/catch-or-return */
 import plugin, { StartFiles, StartPlugin } from '@start/plugin/src/'
 
-export default (glob: string | string[], userEvents?: string[], userOptions?: {}) =>
-  (target: StartPlugin) =>
-    plugin('watch', async ({ logMessage, reporter }) => {
-      const { default: chokidar } = await import('chokidar')
+export default (glob: string | string[], userOptions?: {}) => (target: StartPlugin) =>
+  plugin('watch', async ({ logMessage, reporter }) => {
+    const { default: chokidar } = await import('chokidar')
 
-      const events = userEvents || ['add', 'change']
-      const options = {
-        cwd: process.cwd(),
-        persistent: true,
-        ...userOptions
+    const targetPlugin = await target
+    const events = ['add', 'change']
+    const options = {
+      cwd: process.cwd(),
+      persistent: true,
+      ...userOptions
+    }
+
+    return new Promise<StartFiles>((resolve, reject) => {
+      const initialFiles = []
+      const initialListener = (file) => {
+        initialFiles.push({
+          path: file,
+          data: null,
+          map: null
+        })
       }
 
-      return new Promise<StartFiles>((resolve, reject) => {
-        const initialFiles = []
-        const initialListener = (file) => {
-          initialFiles.push({
-            path: file,
-            data: null,
-            map: null
+      const watcher = chokidar.watch(glob, options)
+
+      watcher.on('add', initialListener)
+      watcher.once('error', reject)
+      watcher.once('ready', async () => {
+        watcher.removeListener('add', initialListener)
+
+        const watchForChanges = () => {
+          events.forEach((event) => {
+            watcher.once(event, async (file) => {
+              try {
+                await targetPlugin({
+                  reporter,
+                  files: [
+                    {
+                      path: file,
+                      data: null,
+                      map: null
+                    }
+                  ]
+                })
+              } finally {
+                watchForChanges()
+              }
+            })
           })
         }
 
-        const watcher = chokidar.watch(glob, options)
-
-        watcher.on('add', initialListener)
-        watcher.once('error', reject)
-        watcher.once('ready', async () => {
-          watcher.removeListener('add', initialListener)
-
-          const watchForChanges = () => {
-            events.forEach((event) => {
-              watcher.once(event, async (file) => {
-                try {
-                  await target({
-                    reporter,
-                    files: [
-                      {
-                        path: file,
-                        data: null,
-                        map: null
-                      }
-                    ]
-                  })
-                } finally {
-                  watchForChanges()
-                }
-              })
-            })
-          }
-
-          try {
-            await target({
-              reporter,
-              files: initialFiles
-            })
-          } finally {
-            watchForChanges()
-            logMessage('watching for changes, press ctrl-c to exit')
-          }
-        })
+        try {
+          await targetPlugin({
+            reporter,
+            files: initialFiles
+          })
+        } finally {
+          watchForChanges()
+          logMessage('watching for changes, press ctrl-c to exit')
+        }
       })
     })
+  })
