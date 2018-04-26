720087b3b9f866b5a5636c63fa71158d93d9f9e0
Kir Belevich <kir@belevi.ch>
2018-04-26 17:41:25 +0200

    💥🛠️ plugin-overwrite: don't produce source maps

--- packages/plugin-overwrite/src/index.ts
+++ packages/plugin-overwrite/src/index.ts
@@ -3,56 +3,17 @@ import plugin from '@start/plugin/src/'
 type WriteFile = (path: string, data: string, options: string, cb: (err: any) => void) => void
 
 export default plugin('overwrite', async ({ files, logFile }) => {
-  const { default: path } = await import('path')
   const { default: makethen } = await import('makethen')
   const { default: gracefulFs } = await import('graceful-fs')
   const writeFile = makethen(gracefulFs.writeFile as WriteFile)
 
   return Promise.all(
-    files.map((file) => {
-      // file.path = /Users/foo/test/packages/beep/src/boop/index.js
-      const writeFiles = []
-      let fileData = file.data || ''
+    files.map((file) =>
+      writeFile(file.path, file.data, 'utf8').then(() => {
+        logFile(file.path)
 
-      // sourcemap
-      if (file.map != null) {
-        // /Users/foo/test/packages/beep/src/boop/index.js -> /Users/foo/test/packages/beep/src/boop/
-        const inDir = path.dirname(file.path)
-        // /Users/foo/test/packages/beep/src/boop/index.js -> index.js
-        const inFile = path.basename(file.path)
-        // /beep/boop/src/beep/index.js -> .js
-        const inExtname = path.extname(file.path)
-        // index.js -> index.js.map
-        const sourcemapFile = inFile + '.map'
-        // /beep/boop/build/beep/index.js -> /beep/boop/build/beep/index.js.map
-        const sourcemapPath = path.join(inDir, sourcemapFile)
-        const sourcemapData = JSON.stringify(file.map)
-
-        // /*# sourceMappingURL=index.css.map */
-        if (inExtname === '.css') {
-          fileData += '\n/*# sourceMappingURL='
-          fileData += sourcemapFile
-          fileData += ' */'
-          // //# sourceMappingURL=index.js.map
-        } else {
-          fileData += '\n//# sourceMappingURL='
-          fileData += sourcemapFile
-        }
-
-        writeFiles.push(
-          writeFile(sourcemapPath, sourcemapData, 'utf8').then(() => {
-            logFile(sourcemapPath)
-          })
-        )
-      }
-
-      writeFiles.push(
-        writeFile(file.path, fileData, 'utf8').then(() => {
-          logFile(file.path)
-        })
-      )
-
-      return Promise.all(writeFiles)
-    })
-  ).then(() => files)
+        return file
+      })
+    )
+  )
 })
