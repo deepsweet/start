b217e3a4b29c8f4d9e4c5610001deb3883a9ce7b
Kir Belevich <kir@belevi.ch>
2018-04-27 11:32:25 +0200

    💄 plugin-sequence

--- packages/plugin-sequence/src/index.ts
+++ packages/plugin-sequence/src/index.ts
@@ -3,11 +3,13 @@ import plugin, { StartPlugin } from '@start/plugin/src/'
 export default (...plugins: StartPlugin[]) =>
   plugin('sequence', ({ files, reporter }) =>
     plugins.reduce(
-      async (prev, next) =>
-        (await next)({
+      async (prev, next) => {
+        const nextRunner = await next
+
+        return nextRunner({
           reporter,
           files: await prev
-        }),
-      Promise.resolve(files)
+        })
+      }, Promise.resolve(files)
     )
   )
