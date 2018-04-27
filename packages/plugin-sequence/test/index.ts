bb49c1565e3dd13c12212351bbc18461e6aff98e
Kir Belevich <kir@belevi.ch>
2018-04-27 18:28:58 +0200

    👾 plugin-sequence: simplify tests

--- packages/plugin-sequence/test/index.ts
+++ packages/plugin-sequence/test/index.ts
@@ -81,37 +81,6 @@ test('plugin-sequence: ok / promise output', async (t) => {
   t.end()
 })
 
-test('plugin-sequence: ok / plugin as promise', async (t) => {
-  const props = {
-    files,
-    reporter: new EventEmitter()
-  }
-  const plugin1 = stub().returns(files)
-  const plugin1Promise = Promise.resolve(plugin1)
-  const plugin2 = stub().resolves(files)
-  const plugin2Promise = Promise.resolve(plugin2)
-
-  const result = await sequence(plugin1Promise, plugin2Promise)(props)
-
-  t.ok(
-    plugin1.calledOnceWith(props),
-    'plugin1 should be called with props'
-  )
-
-  t.ok(
-    plugin2.calledOnceWith(props),
-    'plugin2 should be called with props'
-  )
-
-  t.deepEqual(
-    result,
-    files,
-    'result of sequence should be equal files'
-  )
-
-  t.end()
-})
-
 test('plugin-sequence: error / throw', async (t) => {
   const props = {
     files,
