82a30549bc2d6f7c75712b3bf1a8729f2a503a9f
Kir Belevich <kir@belevi.ch>
2018-04-30 21:23:33 +0200

    👾 cli: add some tests

--- packages/cli/test/lib.ts
+++ packages/cli/test/lib.ts
@@ -1,8 +1,7 @@
 import test from 'tape'
 
-import cliLib from '../src/lib'
-
 test('cli: export', (t) => {
+  const { default: cliLib } = require('../src/lib')
   t.equal(
     typeof cliLib,
     'function',
@@ -11,3 +10,38 @@ test('cli: export', (t) => {
 
   t.end()
 })
+
+test('cli: no reporter', (t) => {
+  const { default: cliLib } = require('../src/lib')
+  const argv = ['foo', 'bar', 'task', 'arg']
+  const options = {
+    file: 'test.js'
+  }
+
+  cliLib(argv, options).catch((error) => {
+    t.ok(
+      /.*reporter.+missing.*/.test(error),
+      'should throw'
+    )
+
+    t.end()
+  })
+})
+
+// test('cli: preset', (t) => {
+//   const { default: cliLib } = require('../src/lib')
+//   const argv = ['foo', 'bar', 'task', 'arg']
+//   const options = {
+//     preset: 'foo',
+//     reporter: 'test-reporter'
+//   }
+//
+//   cliLib(argv, options)
+//     .then(() => {
+//       t.end()
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//       t.end()
+//     })
+// })
