95d1ea654632da3580a71101f7d3c7031a6f47f8
Kir Belevich <kir@belevi.ch>
2018-05-11 14:06:30 +0200

    👾 plugin-env: add tests

--- packages/plugin-env/test/index.ts
+++ packages/plugin-env/test/index.ts
@@ -1,6 +1,14 @@
-import test from 'tape'
+import EventEmitter from 'events'
+import test from 'tape-promise/tape'
+import { spy } from 'sinon'
 
 import env from '../src'
+
+const files = [{
+  path: 'test',
+  data: null,
+  map: null
+}]
 
 test('plugin-env: export', (t) => {
   t.equal(
@@ -10,4 +18,43 @@ test('plugin-env: export', (t) => {
   )
 
   t.end()
+})
+
+test('plugin-env: process.env', (t) => {
+  env('FOO', 'BAR')
+
+  t.equal(
+    process.env.FOO,
+    'BAR',
+    'should set process.env'
+  )
+
+  t.end()
+})
+
+test('plugin-env: files', async (t) => {
+  const reporter = new EventEmitter()
+  const run = env('FOO', 'BAR')
+  const result = await run({ files, reporter })
+
+  t.deepEqual(
+    result,
+    files,
+    'should pass `files` through'
+  )
+})
+
+test('plugin-env: message', async (t) => {
+  const reporter = new EventEmitter()
+  const onMessageSpy = spy()
+
+  reporter.on('message', onMessageSpy)
+
+  const run = env('FOO', 'BAR')
+  const result = await run({ files, reporter })
+
+  t.ok(
+    onMessageSpy.calledOnceWith('env', 'FOO = BAR'),
+    'should log message'
+  )
 })
