bffb183c14e95568553a44c67d1091df39a492fb
Kir Belevich <kir@belevi.ch>
2018-05-11 14:57:56 +0200

    👾 plugin-assert: add tests

--- packages/plugin-assert/test/index.ts
+++ packages/plugin-assert/test/index.ts
@@ -1,6 +1,14 @@
-import test from 'tape'
+import EventEmitter from 'events'
+import test from 'tape-promise/tape'
+import { spy } from 'sinon'
 
 import assert from '../src'
+
+const files = [{
+  path: 'test',
+  data: null,
+  map: null
+}]
 
 test('plugin-assert: export', (t) => {
   t.equal(
@@ -10,4 +18,63 @@ test('plugin-assert: export', (t) => {
   )
 
   t.end()
+})
+
+test('plugin-assert: throw with default message', async (t) => {
+  const reporter = new EventEmitter()
+  const onErrorSpy = spy()
+
+  reporter.on('error', onErrorSpy)
+
+  try {
+    await assert(false)({ files, reporter })
+  } catch (error) {
+    t.ok(
+      onErrorSpy.calledOnce,
+      'should throw assert error only once'
+    )
+
+    t.ok(
+      onErrorSpy.calledWithMatch(
+        'assert',
+        { code: 'ERR_ASSERTION', message: 'false == true' }
+      ),
+      'should throw assert error'
+    )
+  }
+})
+
+test('plugin-assert: throw with custom message', async (t) => {
+  const reporter = new EventEmitter()
+  const onErrorSpy = spy()
+
+  reporter.on('error', onErrorSpy)
+
+  try {
+    await assert(false, 'should be true!')({ files, reporter })
+  } catch (error) {
+    t.ok(
+      onErrorSpy.calledOnce,
+      'should throw assert error only once'
+    )
+
+    t.ok(
+      onErrorSpy.calledWithMatch(
+        'assert',
+        { code: 'ERR_ASSERTION', message: 'should be true!' }
+      ),
+      'should throw assert error'
+    )
+  }
+})
+
+test('plugin-assert: files', async (t) => {
+  const reporter = new EventEmitter()
+  const result = await assert(true)({ files, reporter })
+
+  t.deepEqual(
+    result,
+    files,
+    'should pass `files` through'
+  )
 })
