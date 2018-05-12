62e297c2580f1ca84ad83892fc179e41c6e1a029
Kir Belevich <kir@belevi.ch>
2018-05-12 11:20:26 +0200

    👾 plugin-input-files: add tests

--- packages/plugin-input-files/test/index.ts
+++ packages/plugin-input-files/test/index.ts
@@ -1,8 +1,11 @@
-import test from 'tape'
+import { resolve } from 'path'
+import EventEmitter from 'events'
+import test from 'tape-promise/tape'
+import { stub } from 'sinon'
 
 import inputFiles from '../src'
 
-test('plugin-inputFiles: export', (t) => {
+test('plugin-input-files: export', (t) => {
   t.equal(
     typeof inputFiles,
     'function',
@@ -10,4 +13,67 @@ test('plugin-inputFiles: export', (t) => {
   )
 
   t.end()
+})
+
+test('plugin-input-files: simple', async (t) => {
+  const reporter = new EventEmitter()
+  const targetPluginSpy = stub().returns('test')
+  const files = [
+    '../src/index.ts',
+    '../test/index.ts'
+  ]
+
+  const result = await inputFiles(targetPluginSpy)(...files)({
+    reporter
+  })
+
+  t.ok(
+    targetPluginSpy.calledOnceWith({
+      reporter,
+      files: files.map((file) => ({
+        path: resolve(file),
+        data: null,
+        map: null
+      }))
+    }),
+    'should call plugin with files and props'
+  )
+
+  t.equal(
+    result,
+    'test',
+    'should return called plugin result'
+  )
+})
+
+test('plugin-input-files: async plugin', async (t) => {
+  const reporter = new EventEmitter()
+  const targetPluginSpy = stub().returns('test')
+  const targetPluginPromise = Promise.resolve(targetPluginSpy)
+  const files = [
+    '../src/index.ts',
+    '../test/index.ts'
+  ]
+
+  const result = await inputFiles(targetPluginPromise)(...files)({
+    reporter
+  })
+
+  t.ok(
+    targetPluginSpy.calledOnceWith({
+      reporter,
+      files: files.map((file) => ({
+        path: resolve(file),
+        data: null,
+        map: null
+      }))
+    }),
+    'should call plugin with files and props'
+  )
+
+  t.equal(
+    result,
+    'test',
+    'should return called plugin result'
+  )
 })
