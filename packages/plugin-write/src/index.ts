31754f064283578e8bcd9ec7db8cd4a330427a8e
Kir Belevich <kir@belevi.ch>
2018-05-15 17:24:13 +0200

    🛠️ plugin-write: refactor using `move-path` package

--- packages/plugin-write/src/index.ts
+++ packages/plugin-write/src/index.ts
@@ -7,38 +7,15 @@ export default (outDirRelative: string) =>
     const { default: path } = await import('path')
     const { default: makethen } = await import('makethen')
     const { default: gracefulFs } = await import('graceful-fs')
+    const { default: movePath } = await import('move-path')
     const { default: makeDirP } = await import('mkdirp')
     const makeDir = makethen(makeDirP)
     const writeFile = makethen(gracefulFs.writeFile as WriteFile)
 
     return Promise.all(
       files.map((file) => {
-        // file.path = /Users/foo/test/packages/beep/src/boop/index.js
-        // process.cwd() = /Users/foo/test
-        // outDirRelative = packages/beep/build
-
-        // /Users/foo/test/packages/beep/src/boop/index.js -> index.js
-        const inFile = path.basename(file.path)
-        // /Users/foo/test/packages/beep/src/boop/index.js -> /Users/foo/test/packages/beep/src/boop
-        const inDir = path.dirname(file.path)
-        // /Users/foo/test/packages/beep/src/boop -> packages/beep/src/boop
-        const inDirRelative = path.relative(process.cwd(), inDir)
-        // src/beep -> [ 'packages', 'beep', 'src', 'boop ]
-        const inDirSplit = inDirRelative.split(path.sep)
-        // [ 'packages', 'beep', 'build' ]
-        const outDirSplit = outDirRelative.split(path.sep)
-        // [ 'packages', 'beep', 'src', 'boop ] + [ 'packages', 'beep', 'build' ]
-        const inDirUnique = inDirSplit
-          // [ 'packages', 'beep', 'src', 'boop ] -> [ 'src', 'boop' ]
-          .filter((segment, index) => segment !== outDirSplit[index])
-          // [ 'src', 'boop' ] -> [ 'boop' ]
-          .slice(1)
-          // [ 'boop' ] -> 'boop'
-          .join(path.sep)
-        // packages/beep/build + boop -> /Users/foo/test/packages/beep/build/boop
-        const outDir = path.resolve(outDirRelative, inDirUnique)
-        // packages/beep/build/boop + index.js ->  /Users/foo/test/packages/beep/build/boop/index.js
-        const outFile = path.join(outDir, inFile)
+        const outFile = movePath(file.path, outDirRelative)
+        const outDir = path.dirname(outFile)
 
         return makeDir(outDir)
           .then(() => {
@@ -47,7 +24,8 @@ export default (outDirRelative: string) =>
 
             // sourcemap
             if (file.map != null) {
-            // /beep/boop/src/beep/index.js -> .js
+              const inFile = path.basename(file.path)
+              // /beep/boop/src/beep/index.js -> .js
               const inExtname = path.extname(file.path)
               // index.js -> index.js.map
               const sourcemapFile = inFile + '.map'
