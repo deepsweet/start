7a5d1fdd8450085afbee06c2a0eefaa0524df0e3
Kir Belevich <kir@belevi.ch>
2018-05-25 19:37:14 +0200

    🛠️ plugin-copy: switch to `make-dir` dependency

--- packages/plugin-copy/src/index.ts
+++ packages/plugin-copy/src/index.ts
@@ -3,11 +3,9 @@ import plugin, { StartFile } from '@start/plugin/src/'
 export default (outDirRelative: string) =>
   plugin('copy', async ({ files, logFile }) => {
     const { default: path } = await import('path')
-    const { default: makethen } = await import('makethen')
     const { default: fs } = await import('graceful-fs')
     const { default: movePath } = await import('move-path')
-    const { default: mkdirp } = await import('mkdirp')
-    const makeDir = makethen(mkdirp)
+    const { default: makeDir } = await import('make-dir')
 
     return Promise.all(
       files.map((file) => {
