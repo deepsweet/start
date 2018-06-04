5930faf83ffa0d062f4d5004cb8b4ea25a375336
Kir Belevich <kir@belevi.ch>
2018-06-04 19:36:01 +0200

    🛠️ plugin-decompress: refactor a bit

--- packages/plugin-decompress/src/index.ts
+++ packages/plugin-decompress/src/index.ts
@@ -1,21 +1,17 @@
 import plugin from '@start/plugin/src/'
-import { DecompressOptions } from 'decompress';
+import { DecompressOptions } from 'decompress'
 
 export default (outDirRelative: string, options?: DecompressOptions) =>
-  plugin('decompress', async ({ files, logFile, logMessage }) => {
+  plugin('decompress', async ({ files, logFile }) => {
     const { default: decompress } = await import('decompress')
-    const path = await import('path')
-    
+
     return Promise.all(
-      files.map(async file => {
-        try {
-          logFile(outDirRelative)
-          let decompressedFiles = await decompress(file.path, outDirRelative, options)
-          logMessage(`Decompressed ${path.basename(file.path)} to ${outDirRelative}`)
-          return await { path: outDirRelative, data: null, map: null }
-        } catch (error) {
-          console.error(error)
-        }
+      files.map(async (file) => {
+        let decompressedFiles = await decompress(file.path, outDirRelative, options)
+
+        logPath(file.path)
+
+        return file
       })
     )
   })
