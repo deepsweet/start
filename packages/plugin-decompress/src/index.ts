2a30a679b1fac4f816528cadff36ba11833c39f7
Kir Belevich <kir@belevi.ch>
2018-06-04 21:53:51 +0200

    🛠️ return decompressed files and log paths

--- packages/plugin-decompress/src/index.ts
+++ packages/plugin-decompress/src/index.ts
@@ -3,15 +3,24 @@ import { DecompressOptions } from 'decompress'
 
 export default (outDirRelative: string, options?: DecompressOptions) =>
   plugin('decompress', async ({ files, logFile }) => {
+    const path = await import('path')
     const { default: decompress } = await import('decompress')
 
     return Promise.all(
       files.map(async (file) => {
         let decompressedFiles = await decompress(file.path, outDirRelative, options)
 
-        logFile(file.path)
+        return decompressedFiles.map((decompressedFile) => {
+          const fullPath = path.resolve(outDirRelative, decompressedFile.path)
 
-        return file
+          logFile(fullPath)
+
+          return {
+            path: fullPath,
+            data: null,
+            map: null
+          }
+        })
       })
     )
   })
