719433588144caab34a01b1fe0c3bb38bd7305bb
Kir Belevich <kir@belevi.ch>
2018-06-05 21:40:52 +0200

    🛠️ flatten array of arrays

--- packages/plugin-unpack/src/index.ts
+++ packages/plugin-unpack/src/index.ts
@@ -6,20 +6,22 @@ export default (outDir: string) =>
     const { default: unpack } = await import('decompress')
 
     return Promise.all(
-      files.map(async (file) => {
-        let unpackedFiles = await unpack(file.path, outDir)
+      files
+        .map(async (file) => {
+          let unpackedFiles = await unpack(file.path, outDir)
 
-        return unpackedFiles.map((unpackedFile) => {
-          const fullPath = path.resolve(outDir, unpackedFile.path)
+          return unpackedFiles.map((unpackedFile) => {
+            const fullPath = path.resolve(outDir, unpackedFile.path)
 
-          logFile(fullPath)
+            logFile(fullPath)
 
-          return {
-            path: fullPath,
-            data: null,
-            map: null
-          }
+            return {
+              path: fullPath,
+              data: null,
+              map: null
+            }
+          })
         })
-      })
+        .reduce((result, next) => result.concat(next), [])
     )
   })
