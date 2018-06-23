18c380b7d546bf4427860dec050673b3aebb37a4
Kir Belevich <kir@belevi.ch>
2018-06-23 15:34:42 +0200

    🛠️ plugin-remove: switch to `dleet` dependency

--- packages/plugin-remove/src/index.ts
+++ packages/plugin-remove/src/index.ts
@@ -1,26 +1,14 @@
 import plugin from '@start/plugin/src/'
-import { Options } from 'rimraf'
-
-type RimRaf = (path: string, options: Options, cb: (error: any) => void) => void
 
 export default plugin('remove', async ({ files, logFile }) => {
-  const { default: makethen } = await import('makethen')
-  const { default: rimraf } = await import('rimraf')
-
-  const rimrafP = makethen(rimraf as RimRaf)
-
-  const options: Options = {
-    glob: false
-  }
+  const { default: dleet } = await import('dleet')
 
   return {
     files: await Promise.all(
-      files.map((file) => {
-        return rimrafP(file.path, options).then(() => {
-          logFile(file.path)
+      files.map(async (file) => {
+        await dleet(file.path)
 
-          return file
-        })
+        logFile(file.path)
       })
     )
   }
