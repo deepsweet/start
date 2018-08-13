e421a94fdccdad7bbee691d6c7786a6801d8937d
Kir Belevich <kir@belevi.ch>
2018-08-13 22:22:06 +0200

    🛠 plugin-lib-esm-loader: add second argument to specify a filename

--- packages/plugin-lib-esm-loader/readme.md
+++ packages/plugin-lib-esm-loader/readme.md
@@ -17,7 +17,7 @@ $ npm install --save-dev @start/plugin-lib-esm-loader
 ### Signature
 
 ```ts
-esm(outDirRelative: string)
+esm(outDir: string, filename: string = 'esm-loader.js')
 ```
 
 ### Example
--- packages/plugin-lib-esm-loader/src/index.ts
+++ packages/plugin-lib-esm-loader/src/index.ts
@@ -1,12 +1,12 @@
 import plugin from '@start/plugin/src/'
 
-export default (outDirRelative: string) =>
+export default (outDir: string, filename: string = 'esm-loader.js') =>
   plugin('esm-loader', async ({ logFile }) => {
     const path = await import('path')
     const { default: copie } = await import('copie')
 
     const loaderFilePath = require.resolve('./esm-loader.js')
-    const outFile = path.resolve(outDirRelative, 'esm-loader.js')
+    const outFile = path.resolve(outDir, filename)
 
     await copie(loaderFilePath, outFile)
 
