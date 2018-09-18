3738cbf2e8bcc29e24255b1be109d3dbbdec229e
Kir Belevich <kir.belevich@klarna.com>
2018-09-18 10:33:54 +0200

    ➕ plugin-lib-auto: add `pushCommitsAndTags`

--- packages/plugin-lib-auto/src/index.ts
+++ packages/plugin-lib-auto/src/index.ts
@@ -1,2 +1,10 @@
+import plugin from '@start/plugin/src/'
+
+export const pushCommitsAndTags = plugin('pushCommitsAndTags', async () => {
+  const { pushCommitsAndTags: push } = await import('@auto/git')
+
+  await push()
+})
+
 export * from './repo'
 export * from './workspaces'
