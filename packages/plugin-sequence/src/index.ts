57b502a8a7061f619443bdd94294738fb853af14
psxcode <psxcode@gmail.com>
2019-02-06 11:11:13 +0100

    🛠 refactor types

--- packages/plugin-sequence/src/index.ts
+++ packages/plugin-sequence/src/index.ts
@@ -1,4 +1,19 @@
-import plugin, { StartPlugin, StartReporter, TExtend, TExtend3, TExtend4, TExtend5, TExtend6, TExtend7, TExtend8, TExtend9, TExtend10, TExtend11 } from '@start/plugin/src/'
+/* eslint-disable space-infix-ops */
+import plugin, { StartPlugin, StartReporter, MaybeObject } from '@start/plugin/src/'
+
+export type TExtend <T1 extends MaybeObject, T2 extends MaybeObject> = (T1 extends void ? {} : {
+  [K in Exclude<keyof T1, keyof T2>]: T1[K];
+}) & (T2 extends void ? {} : T2)
+
+export type TExtend3 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject> = TExtend<TExtend<T1, T2>, T3>
+export type TExtend4 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject> = TExtend<TExtend3<T1, T2, T3>, T4>
+export type TExtend5 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject> = TExtend<TExtend4<T1, T2, T3, T4>, T5>
+export type TExtend6 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject> = TExtend<TExtend5<T1, T2, T3, T4, T5>, T6>
+export type TExtend7 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject> = TExtend<TExtend6<T1, T2, T3, T4, T5, T6>, T7>
+export type TExtend8 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject> = TExtend<TExtend7<T1, T2, T3, T4, T5, T6, T7>, T8>
+export type TExtend9 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject> = TExtend<TExtend8<T1, T2, T3, T4, T5, T6, T7, T8>, T9>
+export type TExtend10 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject, T10 extends MaybeObject> = TExtend<TExtend9<T1, T2, T3, T4, T5, T6, T7, T8, T9>, T10>
+export type TExtend11 <T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject, T10 extends MaybeObject, T11 extends MaybeObject> = TExtend<TExtend10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>, T11>
 
 function sequence<P0, R>(p0: StartPlugin<P0, R>): (reporter: StartReporter) => (props?: P0) => Promise<TExtend<P0, R>>
 function sequence<P0, P1, R>(p0: StartPlugin<P0, P1>, p1: StartPlugin<TExtend<P0, P1>, R>): (reporter: StartReporter) => (props?: P0) => Promise<TExtend3<P0, P1, R>>
--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,27 +1,5 @@
 /* eslint-disable space-infix-ops */
-export type TAnyObject = {
-  [key: string]: any
-}
-export type TKeyOf<T> = Exclude<keyof T, number | symbol>
-export type TOmit<T extends {}, K extends PropertyKey> = {
-  [P in Exclude<keyof T, K>]: T[P];
-}
-
 export type MaybeObject = void | {}
-
-export type TExtend<T1 extends MaybeObject, T2 extends MaybeObject> = (T1 extends void ? {} : {
-  [K in Exclude<keyof T1, keyof T2>]: T1[K];
-}) & (T2 extends void ? {} : T2)
-
-export type TExtend3<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject> = TExtend<TExtend<T1, T2>, T3>
-export type TExtend4<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject> = TExtend<TExtend3<T1, T2, T3>, T4>
-export type TExtend5<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject> = TExtend<TExtend4<T1, T2, T3, T4>, T5>
-export type TExtend6<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject> = TExtend<TExtend5<T1, T2, T3, T4, T5>, T6>
-export type TExtend7<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject> = TExtend<TExtend6<T1, T2, T3, T4, T5, T6>, T7>
-export type TExtend8<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject> = TExtend<TExtend7<T1, T2, T3, T4, T5, T6, T7>, T8>
-export type TExtend9<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject> = TExtend<TExtend8<T1, T2, T3, T4, T5, T6, T7, T8>, T9>
-export type TExtend10<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject, T10 extends MaybeObject> = TExtend<TExtend9<T1, T2, T3, T4, T5, T6, T7, T8, T9>, T10>
-export type TExtend11<T1 extends MaybeObject, T2 extends MaybeObject, T3 extends MaybeObject, T4 extends MaybeObject, T5 extends MaybeObject, T6 extends MaybeObject, T7 extends MaybeObject, T8 extends MaybeObject, T9 extends MaybeObject, T10 extends MaybeObject, T11 extends MaybeObject> = TExtend<TExtend10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>, T11>
 
 export type StartReporter = NodeJS.EventEmitter
 
