59875715daeddd7e3d909d7ff11685df667e4229
Kir Belevich <kir@belevi.ch>
2019-04-14 19:34:26 +0200

    🛠 plugin-read: minor refactoring

--- packages/plugin-read/src/index.ts
+++ packages/plugin-read/src/index.ts
@@ -2,13 +2,14 @@ import plugin, { StartDataFile, StartFilesProps } from '@start/plugin/src/'
 
 export default plugin('read', ({ logPath }) => async ({ files }: StartFilesProps) => {
   const { promisify } = await import('util')
-  const gracefulFs = await import('graceful-fs')
-  const readFile = promisify(gracefulFs.readFile)
+  const { readFile } = await import('graceful-fs')
+
+  const pReadFile = promisify(readFile)
 
   return {
     files: await Promise.all(
       files.map(async (file): Promise<StartDataFile> => {
-        const data = await readFile(file.path, 'utf8')
+        const data = await pReadFile(file.path, 'utf8')
 
         logPath(file.path)
 
